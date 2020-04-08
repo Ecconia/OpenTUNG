87e74aa62d943718fe85f5c43fa6f3cc032977a0
Ecconia <eccohirsch@gmail.com>
2020-04-08 02:07:51 +0200

    Adding light calculation

--- basicShader.fs
+++ basicShader.fs
@@ -1,11 +1,41 @@
 #version 330 core
 precision mediump float;
 
-in vec4 transColor;
+in vec4 tColor; //Color (diffuse) of this fragment
+in vec3 tPosition; //Camera position of this fragment
+in vec3 tNormal; //Normal of this fragment
+
+uniform mat4 view2;
 
 out vec4 outColor;
 
 void main()
 {
-    outColor = transColor;
+    vec3 normal = normalize(tNormal);
+
+    vec3 vV = normalize(-tPosition); //Turn the vertex position into a vector to origin
+    vec3 color = vec3(0.0, 0.0, 0.0);
+
+    vec3 sourcePosition = vec3(20.0, 20.0, 20.0);
+    vec3 sourceAmbient = vec3(0.0, 0.0, 0.0);
+    vec3 sourceDiffuse = vec3(1.0, 1.0, 1.0);
+
+    vec3 materialAmbient = vec3(0.0, 0.0, 0.0);
+    vec3 materialDiffuse = tColor.rgb;
+
+    //Loop:
+    vec3 lightPosition = (view2 * vec4(sourcePosition, 1.0)).xyz;
+    vec3 vL = lightPosition - tPosition; //Vector from Vertex to Lightsource
+    float d = length(vL); //Get distance from the Vertex to the Lightsource
+    //float fatt = min(1.0, 1.0 / (sourceC[i].x + sourceC[i].y * d + sourceC[i].z * d * d)); //Disabled.
+    float fatt = 1.0;
+    vL = normalize(vL); //Fix length of vector L
+
+    color += sourceAmbient * materialAmbient
+     + fatt * (
+        sourceDiffuse * materialDiffuse * max(0.0, dot(normal, vL))
+        //+ sourceSpecular[i] * tMaterialSpecular * pow(max(0.0, dot(reflect(-vL, normal), vV)), tMaterialN)
+    );
+
+    outColor = vec4(color, tColor.a); //Alpha
 }
--- basicShader.vs
+++ basicShader.vs
@@ -8,10 +8,17 @@ uniform mat4 projection;
 uniform mat4 view;
 uniform mat4 model;
 
-out vec4 transColor;
+out vec4 tColor;
+out vec3 tPosition;
+out vec3 tNormal;
 
 void main()
 {
-    transColor = vec4(inColor.rgb, 0.5);
-    gl_Position = projection * view * model * vec4(inPosition, 1.0);
+    tColor = vec4(inColor.rgb, 0.5);
+
+    mat4 transformMat = view * model;
+    vec4 transformedPos = transformMat * vec4(inPosition, 1.0);
+    gl_Position = projection * transformedPos; //The position in projection system, to be use for placement
+    tPosition = transformedPos.xyz; //The position in camera system, to be use for light calculation
+    tNormal = normalize((inverse(transpose(transformMat)) * vec4(inNormal, 0.0)).xyz);
 }
