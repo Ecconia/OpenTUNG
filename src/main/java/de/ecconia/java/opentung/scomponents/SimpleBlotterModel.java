cdf62bd60b7c362e867614cffa041652f3ee2de7
Ecconia <eccohirsch@gmail.com>
2020-04-19 22:37:44 +0200

    Fix: Blotter Model
    Color and model was wrong.

--- src/main/java/de/ecconia/java/opentung/scomponents/SimpleBlotterModel.java
+++ src/main/java/de/ecconia/java/opentung/scomponents/SimpleBlotterModel.java
@@ -9,14 +9,14 @@ public class SimpleBlotterModel extends SComponent
 		IntHolder offset = new IntHolder(); //Vertices array offset
 		vertices = new float[3 * 6 * 4 * 9 - 2 * 4 * 9];
 		placeCube(0, 0, 0, 0.15f, 0.15f, 0.15f, 1.0f, 1.0f, 1.0f, offset, null); //Base
-		placeCube(0, 0, -(0.15f + 0.15f), 0.045f, 0.045f, 0.15f, 0.0f, 0.0f, 0.0f, offset, Direction.zP); //Input
-		placeCube(0, 0, 0.15f + 0.06f, 0.075f, 0.075f, 0.06f, 1.0f, 0.0f, 0.0f, offset, Direction.zN); //Output
+		placeCube(0, 0, +(0.15f + 0.15f), 0.045f, 0.045f, 0.15f, 0.0f, 0.0f, 0.0f, offset, Direction.zN); //Input
+		placeCube(0, 0, -(0.15f + 0.06f), 0.075f, 0.075f, 0.06f, 0.0f, 0.0f, 0.0f, offset, Direction.zP); //Output
 		
 		offset = new IntHolder(); //Indices array offset
 		indices = new short[3 * 6 * 6 - 2 * 6];
 		placeCubeIndices(offset, 0, null); //Base
-		placeCubeIndices(offset, 24, Direction.zP); //Input
-		placeCubeIndices(offset, 48 - 4, Direction.zN); //Output
+		placeCubeIndices(offset, 24, Direction.zN); //Input
+		placeCubeIndices(offset, 48 - 4, Direction.zP); //Output
 		
 		upload();
 	}
