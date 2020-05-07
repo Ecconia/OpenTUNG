6d24f2466c164c54ce2d2808d0c4a4434d000dc4
Ecconia <eccohirsch@gmail.com>
2020-05-07 19:12:58 +0200

    Fix: Copy-Paste fail: CrossyIndicator

--- src/main/java/de/ecconia/java/opentung/components/fragments/Line.java
+++ src/main/java/de/ecconia/java/opentung/components/fragments/Line.java
@@ -32,15 +32,15 @@ public class Line extends Meshable
 	public void generateModel(float[] vertices, ModelHolder.IntHolder offsetV, short[] indices, ModelHolder.IntHolder offsetI, ModelHolder.IntHolder indexOffset, ModelHolder.TestModelType type, Vector3 offset)
 	{
 		vertices[offsetV.getAndInc()] = (float) start.getX();
-		vertices[offsetV.getAndInc()] = (float) start.getX();
-		vertices[offsetV.getAndInc()] = (float) start.getX();
+		vertices[offsetV.getAndInc()] = (float) start.getY();
+		vertices[offsetV.getAndInc()] = (float) start.getZ();
 		vertices[offsetV.getAndInc()] = (float) color.getR();
 		vertices[offsetV.getAndInc()] = (float) color.getG();
 		vertices[offsetV.getAndInc()] = (float) color.getB();
 		
 		vertices[offsetV.getAndInc()] = (float) end.getX();
-		vertices[offsetV.getAndInc()] = (float) end.getX();
-		vertices[offsetV.getAndInc()] = (float) end.getX();
+		vertices[offsetV.getAndInc()] = (float) end.getY();
+		vertices[offsetV.getAndInc()] = (float) end.getZ();
 		vertices[offsetV.getAndInc()] = (float) color.getR();
 		vertices[offsetV.getAndInc()] = (float) color.getG();
 		vertices[offsetV.getAndInc()] = (float) color.getB();
