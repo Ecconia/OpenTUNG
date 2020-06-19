a1c3cae9debbe7b72ca9e821aa3840cdb32d3b91
Ecconia <eccohirsch@gmail.com>
2020-06-19 10:35:48 +0200

    Fix: SnappingPeg color in raycast layer
    Was the component color, instead of the ID color.

--- src/main/java/de/ecconia/java/opentung/components/CompSnappingPeg.java
+++ src/main/java/de/ecconia/java/opentung/components/CompSnappingPeg.java
@@ -66,8 +66,17 @@ public class CompSnappingPeg extends Component
 	{
 		if(type == MeshTypeThing.Solid || type == MeshTypeThing.Raycast)
 		{
+			Vector3 color = null;
+			if(type.colorISID())
+			{
+				int id = getRayID();
+				int r = id & 0xFF;
+				int g = (id & 0xFF00) >> 8;
+				int b = (id & 0xFF0000) >> 16;
+				color = new Vector3((float) r / 255f, (float) g / 255f, (float) b / 255f);
+			}
 			//TODO: This is super ungeneric, beware.
-			((CubeFull) getModelHolder().getConnectors().get(0)).generateMeshEntry(vertices, verticesIndex, indices, indicesIndex, vertexCounter, null, getPosition(), getRotation(), getModelHolder().getPlacementOffset(), type);
+			((CubeFull) getModelHolder().getConnectors().get(0)).generateMeshEntry(vertices, verticesIndex, indices, indicesIndex, vertexCounter, color, getPosition(), getRotation(), getModelHolder().getPlacementOffset(), type);
 		}
 	}
 	
