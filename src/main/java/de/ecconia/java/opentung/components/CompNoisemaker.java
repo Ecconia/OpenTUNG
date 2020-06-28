f5c295c4b7dd903d67c488433902eda3e2f484e3
Ecconia <eccohirsch@gmail.com>
2020-06-28 08:56:37 +0200

    Adding Noisemaker color support

--- src/main/java/de/ecconia/java/opentung/components/CompNoisemaker.java
+++ src/main/java/de/ecconia/java/opentung/components/CompNoisemaker.java
@@ -5,6 +5,7 @@ import de.ecconia.java.opentung.components.fragments.Color;
 import de.ecconia.java.opentung.components.fragments.CubeFull;
 import de.ecconia.java.opentung.components.fragments.CubeOpen;
 import de.ecconia.java.opentung.components.fragments.Direction;
+import de.ecconia.java.opentung.components.meta.Colorable;
 import de.ecconia.java.opentung.components.meta.CompContainer;
 import de.ecconia.java.opentung.components.meta.Component;
 import de.ecconia.java.opentung.components.meta.ModelHolder;
@@ -12,14 +13,14 @@ import de.ecconia.java.opentung.math.Vector3;
 import de.ecconia.java.opentung.simulation.SimulationManager;
 import de.ecconia.java.opentung.simulation.Updateable;
 
-public class CompNoisemaker extends Component implements Updateable
+public class CompNoisemaker extends Component implements Updateable, Colorable
 {
 	public static final ModelHolder modelHolder = new ModelHolder();
 	
 	static
 	{
 		modelHolder.setPlacementOffset(new Vector3(0.0, 0.075, 0.0));
-		modelHolder.addSolid(new CubeFull(new Vector3(0.0, 0.36 + 0.3, 0.0), new Vector3(0.24, 0.6, 0.24), Color.noisemakerOFF));
+		modelHolder.addColorable(new CubeFull(new Vector3(0.0, 0.36 + 0.3, 0.0), new Vector3(0.24, 0.6, 0.24), Color.noisemakerOFF));
 		modelHolder.addPeg(new CubeOpen(new Vector3(0.0, 0.18, 0.0), new Vector3(0.1, 0.36, 0.1), Direction.YPos));
 	}
 	
@@ -52,5 +53,15 @@ public class CompNoisemaker extends Component implements Updateable
 	@Override
 	public void update(SimulationManager simulation)
 	{
+		boolean on = pegs.get(0).getCluster().isActive();
+		simulation.setColor(colorID, on ? Color.noisemakerON : Color.noisemakerOFF);
+	}
+	
+	private int colorID;
+	
+	@Override
+	public void setColorID(int id, int colorID)
+	{
+		this.colorID = colorID;
 	}
 }
